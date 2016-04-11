0acbae73510e1f6de16aefd5eaa968d055acdfa6
tiridevries <tiridevries13@hotmail.com>
2016-04-11 13:05:56 +0200

    dnksj
    KENDKJ

---
+++ view/Aanwezigen.php
@@ -0,0 +1,89 @@
+<?php include "../view/includes/header.php"; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<link rel="stylesheet" href="../css/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+      
+      <div class="container wf-content">
+          <div class="row">
+            <div class="col-md-2 overzicht-les"><h4>Informatiestromen Les 3</h4></div>
+            <div class="col-md-10"></div>
+            <div class="col-md-2"></div>
+          </div>
+          
+          <div class="row">
+              <div class="col-md-1"></div>
+              <div class="col-md-10">
+                  <table class="table-bordered">
+                    <tr>
+                     <td class="o aanwezig"><br><img src="../css/1.png" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                     <td class="o aanwezig"><br><img src="../css/2.png" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                     <td class="o aanwezig"><br><img src="../css/3.png" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                     <td class="o aanwezig"><br><img src="../css/4.png" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                     <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                     <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                     <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                     <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    </tr>
+                    <tr>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    </tr>
+                    <tr>
+                     <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+					</tr>
+                    <tr>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    </tr>
+                    <tr>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    <td class="o"><br><img src="../css/5.jpe" alt="" width="80" height="90"><br>naam<br>studentnummer</td>
+                    </tr>
+                    
+                  </table>
+              </div>
+              <div class="col-md-1"></div>
+          </div>
+      </div>
+
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+</body>
+</html>
+
