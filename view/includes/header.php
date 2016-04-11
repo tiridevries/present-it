8798121c09e69a41cceab9a042cd538676288124
tiridevries <tiridevries13@hotmail.com>
2016-04-11 12:43:33 +0200

    blub
    index.html veranderd naar index.php + header.php.

---
+++ view/includes/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<link rel="stylesheet" href="../css/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <br>   
+   <div class="container-fluid">
+    
+
+                <nav class="navbar navbar-default">
+
+                      <div class="container-fluid">
+                            <img src="../css/logo.png" class="logo" alt="wf-logo" width="240" height="37">
+                                      <div class="navbar-header">
+                                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                                  <span class="icon-bar"></span>
+                                                  <span class="icon-bar"></span>
+                                                  <span class="icon-bar"></span> 
+                                            </button>
+                                      </div>
+    
+                                      <div class="collapse navbar-collapse" id="myNavbar">
+                                            <ul class="nav navbar-nav">
+                                                  <li><a href="#"><b>Home</b></a></li>
+                                                  <li><a href="#"><b>Aanwezigen</b></a></li>
+                                                  <li><a href="#"><b>Statistieken</b></a></li> 
+                                            </ul>
+    
+                                            <ul class="nav navbar-nav navbar-right">
+                                                  <li><a href="#"><b>Uitloggen</b> <span class="glyphicon glyphicon-log-out"></a></li> 
+                                            </ul>
+                                      </div>
+                      </div>
+              </nav>
+      </div>
+	  
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+</body>
+</html>	  
